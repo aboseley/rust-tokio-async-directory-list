470c03b980e83f5301654caeea26e78d0d399dfa
Adam Boseley <adam.boseley@gmail.com>
2021-10-31 08:19:31 +1000

    Use tokio

--- src/main.rs
+++ src/main.rs
@@ -1,76 +1,39 @@
-use std::fs::DirEntry;
-use std::path::Path;
-use std::path::PathBuf;
-
 use anyhow::Result;
-#[allow(unused_imports)]
-use itertools::Itertools;
-use serde::Serialize;
+use std::path::Path;
+use tokio_stream::StreamExt;
 
-#[derive(Serialize, Debug)]
-struct LogFileNames(Vec<String>);
+async fn get_filenames(in_path: &Path, with_ext: &str) -> Result<Vec<String>> {
+    use tokio::fs;
+    use tokio_stream::wrappers::ReadDirStream;
 
-impl LogFileNames {
-    pub fn new() -> LogFileNames {
-        LogFileNames(vec![])
+    let matching_extension = |entry: &fs::DirEntry| -> bool {
+        entry
+            .path()
+            .extension()
+            .map(|s| s == with_ext)
+            .unwrap_or(false)
+    };
+
+    fn to_filenames(entry: fs::DirEntry) -> String {
+        entry.path().display().to_string()
     }
-}
 
-fn is_logfile(entry: &DirEntry) -> bool {
-    entry
-        .path()
-        .extension()
-        .map(|s| s == "log")
-        .unwrap_or(false)
-}
+    let dir = fs::read_dir(in_path).await?;
+    let dir_stream = ReadDirStream::new(dir);
 
-fn get_log_filenames() -> Result<Vec<String>> {
-    Ok(std::fs::read_dir("/tmp")?
-        .into_iter()
+    Ok(dir_stream
         .filter_map(Result::ok)
-        .filter(is_logfile)
-        .map(|d| d.path().display().to_string())
-        .collect())
-}
-
-fn have_log_name(entry: DirEntry) -> Option<String> {
-    match entry.path().extension() {
-        Some(ext) => {
-            if (ext == "log") {
-                Some(entry.path().display().to_string())
-            } else {
-                None
-            }
-        }
-        _ => None,
-    }
-}
-
-fn get_log_filenames2() -> Result<Vec<String>> {
-    Ok(std::fs::read_dir("/tmp")?
-        .into_iter()
-        .filter_map(Result::ok)
-        .filter_map(have_log_name)
-        .collect())
-}
-
-fn get_log_filenames3() -> Result<Vec<String>> {
-    // flatmap is more general than filter_map
-    Ok(std::fs::read_dir("/tmp")?
-        .into_iter()
-        .filter_map(Result::ok)
-        .flat_map(have_log_name)
-        .collect())
+        .filter(matching_extension)
+        .map(to_filenames)
+        .collect()
+        .await)
 }
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    println!("Hello, world!");
-
-    if let Ok(log_files) = tokio::task::spawn_blocking(get_log_filenames).await? {
-        let log_files_json = serde_json::to_string(&log_files).unwrap();
-        println!("{}", log_files_json);
-    }
+    let filenames = get_filenames(Path::new("/tmp"), "log").await?;
+    let filenames_as_json = serde_json::to_string(&filenames)?;
+    println!("{}", filenames_as_json);
 
     return Ok(());
 }
